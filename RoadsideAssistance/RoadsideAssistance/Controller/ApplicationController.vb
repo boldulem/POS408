dc99570a1ff20835519d5d845f37432cfbf66128
Steven Cline <boldulem@gmail.com>
2013-11-23 17:26:13 -0700

    Application Manager updated with stub

--- RoadsideAssistance/RoadsideAssistance/Controller/ApplicationController.vb
+++ RoadsideAssistance/RoadsideAssistance/Controller/ApplicationController.vb
@@ -1,3 +1,40 @@
-﻿Public Class ApplicationController : Inherits NotifyBase
+﻿'Purpose of this component is to provide validation for the between the gui and the datamanager
+'It needs the variable and contructor to that initializes them.
+Public Class ApplicationController : Inherits NotifyBase
+    Public Sub ApplicationController()
+
+    End Sub
+
+    Public Sub InitializeProviders()
+
+    End Sub
+
+    Public Sub InitializeCustomers()
+
+    End Sub
+
+    Public Sub InitializeContacts()
+
+    End Sub
+
+    Public Sub SaveProvider()
+
+    End Sub
+
+    Public Sub SaveServiceCall()
+
+    End Sub
+
+    Public Sub SaveStatus()
+
+    End Sub
+
+    Public Sub SaveCustomer()
+
+    End Sub
+
+    Public Sub SaveContact()
+
+    End Sub
 
 End Class
